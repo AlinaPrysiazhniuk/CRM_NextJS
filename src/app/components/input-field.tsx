da1fa04df304b46070236a85a6610763fbe1642c
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-07-15 12:45:07 +0300

    build

--- src/app/components/input-field.tsx
+++ src/app/components/input-field.tsx
@@ -3,16 +3,20 @@
 import React from 'react';
 import { Field } from 'formik';
 
-export interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  label?: string;
-  id?: string;
+export interface InputFieldProps {
+    label?: string;
+    placeholder?: string;
+    name: string;
+    type?: string;
+    as?: 'input' | 'select' | 'textarea'; // додано тут
+    required?: boolean;
 }
 
-export default function InputField({label,id, ...rest}: InputFieldProps)
-{
-  return (
-   <div className="flex flex-col">{label && <label htmlFor={id} className="mb-2 text-base color-gray-900">{label}</label> }
-   <Field {...rest} id={id} className="p-3 h-11 text-sm rounded border border-gray-300 shadow" />
-   </div>
-  );
+export default function InputField({ label, as = 'input', ...rest }: InputFieldProps) {
+    return (
+        <div className="flex flex-col">
+            {label && <label htmlFor={rest.name} className="mb-2 text-base color-gray-900">{label}</label>}
+            <Field as={as} {...rest} className="p-3 h-11 text-sm rounded border border-gray-300 shadow" />
+        </div>
+    );
 }
